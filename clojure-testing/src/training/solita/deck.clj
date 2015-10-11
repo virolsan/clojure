6e81f5503cbb5a861140fc9b485a2502490636e3
Santtu Virolainen <santtu.virolainen@solita.fi>
2015-10-11 19:40:43 +0300

    added some base functions

--- clojure-testing/src/training/solita/deck.clj
+++ clojure-testing/src/training/solita/deck.clj
@@ -5,11 +5,13 @@
    \C "club"
    \D "diamond"
    \S "spade"})
+(def deck-values
+  (into [] (concat (range 2 11) [\J \Q \K \A])))
 
 (defn make-deck []
   (for [suit (keys suit-names)
-        value (concat (range 2 11) [\J \Q \K \A])]
-    {:suit suit :value value :rank 1}))
+        value deck-values]
+    {:suit suit :value value :rank (+ 2 (.indexOf deck-values value))}))
 
 (defn get-vals [hand col]
   (reduce #(conj %1 (get %2 col)) [] hand))
@@ -23,40 +25,50 @@
 (defn has-flush? [hand]
   (= (count (set (get-vals hand :suit))) 1))
 
-(defn resolve-better-hand [a b]
-  (let [a-points (resolve-points a)
-        b-points (resolve-points b)]
-  (- a-points b-points)))
-
 (defn resolve-points [hand]
   (let [p1 (if (has-pair? hand) 2 0)
         p2 (if (has-straight? hand) 3 0)
         p3 (if (has-flush? hand) 4 0)]
+  (println (get-vals hand :value) ": " (+ p1 p2 p3) "pts")
   (+ p1 p2 p3)))
+
+(defn resolve-better-hand [a b]
+  (let [a-points (resolve-points a)
+        b-points (resolve-points b)]
+  (if (> a-points b-points) a b)))
+
+
+(defn deal-cards[players]
+  (let [deck (shuffle (make-deck))]
+    (take players (partition 5 deck))))
+
+(let [game (deal-cards 3)]
+  (reduce resolve-better-hand game))
+
+
 
 ; testausta
 
-(sort (get-vals (take 3 (make-deck)) :value))
-(reduce - (sort > (get-vals (take 3 (make-deck)) :value)))
-
 (make-deck)
-(shuffle (make-deck))
-(take 5 (make-deck))
-(get-vals (take 5 (make-deck)) :rank)
+(deal-cards 1)
 
-(has-pair? (take 14 (make-deck)))
-(has-straight? (take 5 (make-deck)))
-(has-flush? (take 5 (make-deck)))
+(has-pair? (nth (deal-cards 1) 0))
+(has-straight? (nth (deal-cards 1) 0))
+(has-flush? (nth (deal-cards 1) 0))
 
-(resolve-points (take 5 (shuffle (make-deck))))
-(resolve-better-hand (take 5 (shuffle (make-deck))) (take 2 (make-deck)))
+(resolve-points (nth (deal-cards 1) 0))
+(reduce resolve-better-hand (deal-cards 2))
+
+
 
 ; muita kokeiluja
 
-(filter has-pair (list (take 2 (make-deck)) (take 14 (make-deck))))
+(filter has-pair? (list (take 2 (make-deck)) (take 14 (make-deck))))
 
 (reduce #(conj %1 (get %2 :rank)) [] (take 5 (make-deck)))
 
 (distinct? [1 2 1])
 (apply distinct? [1 2 1])
 (apply (complement distinct?) [1 2 1])
+
+(take 13 (iterate inc 2))
