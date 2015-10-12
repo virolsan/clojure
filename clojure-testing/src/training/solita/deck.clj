514e5fbe811a3750fe65d1b06df4705b1d42f4c8
Santtu Virolainen <santtu.virolainen@solita.fi>
2015-10-12 12:45:47 +0300

    value system improved

--- clojure-testing/src/training/solita/deck.clj
+++ clojure-testing/src/training/solita/deck.clj
@@ -13,24 +13,48 @@
         value deck-values]
     {:suit suit :value value :rank (+ 2 (.indexOf deck-values value))}))
 
+(defn deal-cards[cards players]
+  (let [deck (shuffle (make-deck))]
+    (take players (partition cards deck))))
+
 (defn get-vals [hand col]
   (reduce #(conj %1 (get %2 col)) [] hand))
 
-(defn has-pair? [hand]
-  (apply (complement distinct?) (get-vals hand :value)))
+(defn group-hand-by-count [hand key]
+   (group-by count (partition-by identity (sort (get-vals hand key)))))
+
+(defn max-kind [hand]
+  (apply max (keys (group-hand-by-count hand :rank))))
+
+(defn has-two-pairs? [hand]
+  (= 2 (count ((group-hand-by-count hand :rank) 2))))
 
 (defn has-straight? [hand]
-  (= (reduce - (sort (get-vals hand :rank))) -4))
+  (let [ranks (sort (get-vals hand :rank))]
+    (= 4 (- (nth ranks 4) (nth ranks 0)))))
 
 (defn has-flush? [hand]
-  (= (count (set (get-vals hand :suit))) 1))
+  (not (empty? ((group-hand-by-count hand :suit) 5))))
 
+; Ranking:
+;  1 = royal flush     10 pts
+;  2 = straight flush   9 pts
+;  3 = four of a kind   8 pts
+;  4 = full house       7 pts
+;  5 = flush            6 pts
+;  6 = straight         5 pts
+;  7 = three of a kind  4 pts
+;  8 = two pairs        3 pts
+;  9 = one pair         2 pts
+; 10 = high card
 (defn resolve-points [hand]
-  (let [p1 (if (has-pair? hand) 2 0)
-        p2 (if (has-straight? hand) 3 0)
-        p3 (if (has-flush? hand) 4 0)]
-  (println (get-vals hand :value) ": " (+ p1 p2 p3) "pts")
-  (+ p1 p2 p3)))
+  (max
+  (if (= 2 (max-kind hand)) 2 0)
+  (if (has-two-pairs? hand) 3 0)
+  (if (= 3 (max-kind hand)) 4 0)
+  (if (has-straight? hand)  5 0)
+  (if (has-flush? hand)     6 0)
+  (if (= 4 (max-kind hand)) 8 0)))
 
 (defn resolve-better-hand [a b]
   (let [a-points (resolve-points a)
@@ -38,26 +62,36 @@
   (if (> a-points b-points) a b)))
 
 
-(defn deal-cards[players]
-  (let [deck (shuffle (make-deck))]
-    (take players (partition 5 deck))))
-
-(let [game (deal-cards 3)]
-  (reduce resolve-better-hand game))
+(let [hands (deal-cards 5 3)]
+  (reduce resolve-better-hand hands))
 
 
 
 ; testausta
 
 (make-deck)
-(deal-cards 1)
+(deal-cards 5 1)
 
-(has-pair? (nth (deal-cards 1) 0))
-(has-straight? (nth (deal-cards 1) 0))
-(has-flush? (nth (deal-cards 1) 0))
+(group-hand-by-count (nth (deal-cards 5 1) 0) :rank)
 
-(resolve-points (nth (deal-cards 1) 0))
-(reduce resolve-better-hand (deal-cards 2))
+(has-straight? '( {:suit \D, :value 4, :rank 4}
+                  {:suit \C, :value 3, :rank 3}
+                  {:suit \C, :value 6, :rank 6}
+                  {:suit \C, :value 5, :rank 5}
+                  {:suit \S, :value 7, :rank 7}))
+(has-flush? '( {:suit \C, :value 4, :rank 4}
+               {:suit \C, :value \A, :rank 14}
+               {:suit \C, :value 3, :rank 3}
+               {:suit \C, :value 6, :rank 6}
+               {:suit \C, :value 7, :rank 7}))
+(has-two-pairs? '( {:suit \D, :value 4, :rank 4}
+                   {:suit \C, :value \A, :rank 14}
+                   {:suit \C, :value 6, :rank 6}
+                   {:suit \C, :value 6, :rank 6}
+                   {:suit \S, :value 4, :rank 4}))
+
+(resolve-points (nth (deal-cards 5 1) 0))
+(reduce resolve-better-hand (deal-cards 5 2))
 
 
 
