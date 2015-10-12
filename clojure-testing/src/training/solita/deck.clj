805fe233e099717016f3fb540b555f9277db4a47
Santtu Virolainen <santtu.virolainen@solita.fi>
2015-10-12 16:14:26 +0300

    Value calculation improved, tests added

--- clojure-testing/src/training/solita/deck.clj
+++ clojure-testing/src/training/solita/deck.clj
@@ -20,14 +20,27 @@
 (defn get-vals [hand col]
   (reduce #(conj %1 (get %2 col)) [] hand))
 
+(defn partition-hand [hand key]
+   (partition-by identity (sort (get-vals hand key))))
+
 (defn group-hand-by-count [hand key]
-   (group-by count (partition-by identity (sort (get-vals hand key)))))
+   (group-by count (partition-hand hand key)))
 
 (defn max-kind [hand]
   (apply max (keys (group-hand-by-count hand :rank))))
 
+
+(defn has-pair? [hand]
+  (= 2 (max-kind hand)))
+
+(defn has-three-of-a-kind? [hand]
+  (= 3 (max-kind hand)))
+
+(defn has-four-of-a-kind? [hand]
+  (= 4 (max-kind hand)))
+
 (defn has-two-pairs? [hand]
-  (= 2 (count ((group-hand-by-count hand :rank) 2))))
+  (= 2 (count (filter #(> (count %) 1) (partition-hand hand :rank)))))
 
 (defn has-straight? [hand]
   (let [ranks (sort (get-vals hand :rank))]
@@ -49,12 +62,17 @@
 ; 10 = high card
 (defn resolve-points [hand]
   (max
-  (if (= 2 (max-kind hand)) 2 0)
-  (if (has-two-pairs? hand) 3 0)
-  (if (= 3 (max-kind hand)) 4 0)
-  (if (has-straight? hand)  5 0)
-  (if (has-flush? hand)     6 0)
-  (if (= 4 (max-kind hand)) 8 0)))
+    (if (has-pair? hand)            2 0)
+    (if (has-two-pairs? hand)       3 0)
+    (if (has-three-of-a-kind? hand) 4 0)
+    (if (has-straight? hand)        5 0)
+    (if (has-flush? hand)           6 0)
+    (if (and (has-three-of-a-kind? hand) 
+             (has-two-pairs? hand ))  7 0)
+    (if (has-four-of-a-kind? hand)  8 0) 
+    (if (and (has-flush? hand) 
+             (has-straight? hand))  9 0)
+  ))
 
 (defn resolve-better-hand [a b]
   (let [a-points (resolve-points a)
@@ -72,32 +90,14 @@
 (make-deck)
 (deal-cards 5 1)
 
-(group-hand-by-count (nth (deal-cards 5 1) 0) :rank)
-
-(has-straight? '( {:suit \D, :value 4, :rank 4}
-                  {:suit \C, :value 3, :rank 3}
-                  {:suit \C, :value 6, :rank 6}
-                  {:suit \C, :value 5, :rank 5}
-                  {:suit \S, :value 7, :rank 7}))
-(has-flush? '( {:suit \C, :value 4, :rank 4}
-               {:suit \C, :value \A, :rank 14}
-               {:suit \C, :value 3, :rank 3}
-               {:suit \C, :value 6, :rank 6}
-               {:suit \C, :value 7, :rank 7}))
-(has-two-pairs? '( {:suit \D, :value 4, :rank 4}
-                   {:suit \C, :value \A, :rank 14}
-                   {:suit \C, :value 6, :rank 6}
-                   {:suit \C, :value 6, :rank 6}
-                   {:suit \S, :value 4, :rank 4}))
+(keys (group-hand-by-count (nth (deal-cards 5 1) 0) :rank))
+(count (group-hand-by-count (nth (deal-cards 5 1) 0) :rank))
 
 (resolve-points (nth (deal-cards 5 1) 0))
 (reduce resolve-better-hand (deal-cards 5 2))
 
 
-
 ; muita kokeiluja
-
-(filter has-pair? (list (take 2 (make-deck)) (take 14 (make-deck))))
 
 (reduce #(conj %1 (get %2 :rank)) [] (take 5 (make-deck)))
 
--- clojure-testing/test/training/solita/deck_test.clj
+++ clojure-testing/test/training/solita/deck_test.clj
@@ -2,8 +2,111 @@
   (:use [training.solita.deck]
         [midje.sweet]))
 
-(facts
-  (fact (count (make-deck)) => 52)
-  (fact (has-pair (take 5 (make-deck))) => false)
-  (fact (has-pair (make-deck)) => true))
+(facts "General"
+  (fact "Size of deck" 
+        (count (make-deck)) => 52)
+)
+
+(facts "Resolve hands"
+  (fact "Single pair"
+        (has-pair? '( {:suit \D, :value 4, :rank 4}
+                      {:suit \C, :value \A, :rank 14}
+                      {:suit \C, :value 6, :rank 6}
+                      {:suit \C, :value 5, :rank 5}
+                      {:suit \S, :value 4, :rank 4})) => true)
+  (fact "Three of a kind"
+        (has-three-of-a-kind? '( {:suit \D, :value 4, :rank 4}
+                                 {:suit \C, :value \A, :rank 14}
+                                 {:suit \C, :value 4, :rank 4}
+                                 {:suit \C, :value 5, :rank 5}
+                                 {:suit \S, :value 4, :rank 4})) => true)
+  (fact "Four of a kind"
+        (has-four-of-a-kind? '( {:suit \D, :value 4, :rank 4}
+                                {:suit \C, :value 4, :rank 4}
+                                {:suit \C, :value 4, :rank 4}
+                                {:suit \C, :value 5, :rank 5}
+                                {:suit \S, :value 4, :rank 4})) => true)
+  (fact "Two pairs"
+        (has-two-pairs? '( {:suit \D, :value 4, :rank 4}
+                           {:suit \C, :value \A, :rank 14}
+                           {:suit \C, :value 6, :rank 6}
+                           {:suit \C, :value 6, :rank 6}
+                           {:suit \S, :value 4, :rank 4})) => true)
+  (fact "Full house has Two pairs"
+        (has-two-pairs? '( {:suit \D, :value 4, :rank 4}
+                           {:suit \C, :value 4, :rank 4}
+                           {:suit \C, :value 6, :rank 6}
+                           {:suit \C, :value 6, :rank 6}
+                           {:suit \S, :value 4, :rank 4})) => true)
+  (fact "Straight"
+        (has-straight? '( {:suit \D, :value 4, :rank 4}
+                          {:suit \C, :value 3, :rank 3}
+                          {:suit \C, :value 6, :rank 6}
+                          {:suit \C, :value 5, :rank 5}
+                          {:suit \S, :value 7, :rank 7})) => true)
+  (fact "Flush"
+        (has-flush? '( {:suit \C, :value 4, :rank 4}
+                       {:suit \C, :value \A, :rank 14}
+                       {:suit \C, :value 3, :rank 3}
+                       {:suit \C, :value 6, :rank 6}
+                       {:suit \C, :value 7, :rank 7})) => true)
+)
+
+  
+(facts "Resolve points"
+  (fact "High card"
+        (resolve-points '( {:suit \D, :value 4, :rank 4}
+                           {:suit \C, :value \A, :rank 14}
+                           {:suit \C, :value 6, :rank 6}
+                           {:suit \C, :value 5, :rank 5}
+                           {:suit \S, :value 8, :rank 8})) => 0)
+  (fact "Single pair"
+        (resolve-points '( {:suit \D, :value 4, :rank 4}
+                           {:suit \C, :value \A, :rank 14}
+                           {:suit \C, :value 6, :rank 6}
+                           {:suit \C, :value 5, :rank 5}
+                           {:suit \S, :value 4, :rank 4})) => 2)
+  (fact "Two pairs"
+        (resolve-points '( {:suit \D, :value 4, :rank 4}
+                           {:suit \C, :value \A, :rank 14}
+                           {:suit \C, :value 6, :rank 6}
+                           {:suit \C, :value 6, :rank 6}
+                           {:suit \S, :value 4, :rank 4})) => 3)
+  (fact "Three of a kind"
+        (resolve-points '( {:suit \D, :value 4, :rank 4}
+                           {:suit \C, :value \A, :rank 14}
+                           {:suit \C, :value 4, :rank 4}
+                           {:suit \C, :value 5, :rank 5}
+                           {:suit \S, :value 4, :rank 4})) => 4)
+  (fact "Straight"
+        (resolve-points '( {:suit \D, :value 4, :rank 4}
+                           {:suit \C, :value 3, :rank 3}
+                           {:suit \C, :value 6, :rank 6}
+                           {:suit \C, :value 5, :rank 5}
+                           {:suit \S, :value 7, :rank 7})) => 5)
+  (fact "Flush"
+        (resolve-points '( {:suit \C, :value 4, :rank 4}
+                           {:suit \C, :value \A, :rank 14}
+                           {:suit \C, :value 3, :rank 3}
+                           {:suit \C, :value 6, :rank 6}
+                           {:suit \C, :value 7, :rank 7})) => 6)
+  (fact "Full house"
+        (resolve-points '( {:suit \D, :value 4, :rank 4}
+                           {:suit \C, :value \A, :rank 14}
+                           {:suit \C, :value 4, :rank 4}
+                           {:suit \C, :value \A, :rank 14}
+                           {:suit \S, :value 4, :rank 4})) => 7)
+  (fact "Four of a kind"
+        (resolve-points '( {:suit \D, :value 4, :rank 4}
+                           {:suit \C, :value 4, :rank 4}
+                           {:suit \C, :value 4, :rank 4}
+                           {:suit \C, :value 5, :rank 5}
+                           {:suit \S, :value 4, :rank 4})) => 8)
+  (fact "Straight flush"
+        (resolve-points '( {:suit \C, :value 4, :rank 4}
+                           {:suit \C, :value 3, :rank 3}
+                           {:suit \C, :value 6, :rank 6}
+                           {:suit \C, :value 5, :rank 5}
+                           {:suit \C, :value 7, :rank 7})) => 9)
+)
 
